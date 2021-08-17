dbb3120b323419cfa96311725c807efb6f074ae1
xMakcRrR <makc-rybin@mail.ru>
2021-08-17 12:43:36 +0300

    - added new sounds - changed permissions request logic

--- app/src/main/java/com/bytersoundboard/ByterButtonsPage.java
+++ app/src/main/java/com/bytersoundboard/ByterButtonsPage.java
@@ -1,18 +1,13 @@
 package com.bytersoundboard;
 
-import android.app.Fragment;
-import android.content.ContentResolver;
-import android.content.ContentValues;
+import android.Manifest;
+import androidx.fragment.app.Fragment;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.media.RingtoneManager;
+import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
@@ -23,18 +18,18 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.BufferedInputStream;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 
 
 public class ByterButtonsPage extends Fragment {
+    private static final int REQUEST_STORE_PERMISSION_CODE = 1;
     ArrayList<SoundButton> soundButtons = new ArrayList<SoundButton>();
     ByterAdapter byterAdapter;
 
@@ -44,9 +39,13 @@ public class ByterButtonsPage extends Fragment {
         String[] buttons = {getString(R.string.hi), getString(R.string.bye),
                 getString(R.string.im_danila), getString(R.string.aboba),
                 getString(R.string.ny_blin), getString(R.string.chto_ze_delat),
-                getString(R.string.eli_pali), getString(R.string.chin_chopa)};
+                getString(R.string.eli_pali), getString(R.string.chin_chopa),
+                getString(R.string.byter_lager_gaming), getString(R.string.fu),
+                getString(R.string.razrivnaya), getString(R.string.slabenko),
+                getString(R.string.tyazeliyslychay)};
         int[] _ids = {R.raw.hi, R.raw.bye, R.raw.im_danila, R.raw.aboba, R.raw.ny_blin,
-                R.raw.chto_ze_delat, R.raw.eli_pali, R.raw.chin_chopa};
+                R.raw.chto_ze_delat, R.raw.eli_pali, R.raw.chin_chopa, R.raw.byterlagergaming,
+                R.raw.fu, R.raw.razrivnaya, R.raw.slabenko, R.raw.tyazeliyslychay};
 
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.page_list, null);
@@ -79,11 +78,19 @@ public class ByterButtonsPage extends Fragment {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         int itemId = item.getItemId();
         if (itemId == R.id.share) {
-            Toast.makeText(getActivity(), "" + getString(R.string.share_toast), Toast.LENGTH_SHORT).show();
-            share(byterAdapter.getSoundId((int) info.id));
+            if (!CheckPermissions()) {
+                requestPermissions();
+            } else {
+                //Toast.makeText(getActivity(), "" + getString(R.string.share_toast), Toast.LENGTH_SHORT).show();
+                share(byterAdapter.getSoundId((int) info.id));
+            }
         } else if (itemId == R.id.setOnCall) {
-            Toast.makeText(getActivity(), "" + getString(R.string.go_to_medic_not),
-                    Toast.LENGTH_SHORT).show();
+            if (!CheckPermissions()) {
+                requestPermissions();
+            } else {
+                Toast.makeText(getActivity(), "" + getString(R.string.go_to_medic_not),
+                        Toast.LENGTH_SHORT).show();
+            }
         }
 
         return true;
@@ -100,12 +107,13 @@ public class ByterButtonsPage extends Fragment {
             File byterDir = new File(""+Environment.getExternalStorageDirectory()+"/byter_sounds/");
 
             if(!byterDir.exists()) {
-                Toast.makeText(getActivity(), "aboba", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getActivity(), "aboba", Toast.LENGTH_LONG).show();
                 byterDir.mkdirs();
             }
 
             fileOutputStream = new FileOutputStream(
-                    new File(Environment.getExternalStorageDirectory()+"/byter_sounds/", "sound.mp3"));
+                    new File(Environment.getExternalStorageDirectory()+"/byter_sounds/",
+                            "sound.mp3"));
 
             byte[] buffer = new byte[1024];
             int length;
@@ -128,5 +136,22 @@ public class ByterButtonsPage extends Fragment {
                 Uri.parse("file://" + Environment.getExternalStorageDirectory() + "/byter_sounds/sound.mp3" ));
         intent.setType("application/*");
         startActivity(Intent.createChooser(intent, "Share sound"));
+    }
+
+    private void requestPermissions() {
+        // this method is used to request the permission.
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORE_PERMISSION_CODE);
+    }
+
+    public boolean CheckPermissions() {
+        // this method is used to check permission
+        int result = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED;
     }
 }
